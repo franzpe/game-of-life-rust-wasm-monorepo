1f0186e612a2c988cfe3131a264863affa1e53dd
František Poboček <pobocekfrantisek@gmail.com>
2023-01-27 12:20:40 +0100

    fps

--- packages/app/src/UniverseGame.tsx
+++ packages/app/src/UniverseGame.tsx
@@ -1,25 +1,30 @@
 import { useEffect, useRef } from 'react'
+
 import { Universe } from 'wasm'
 import { Wasm } from './hooks/useWasm'
 
 type Props = {
   universe: Universe
   wasm: Wasm
+  fps?: number
 }
 
-const UniverseGame = ({ universe }: Props) => {
+const FPS = 45
+
+const UniverseGame = ({ universe, fps = FPS }: Props) => {
   const preRef = useRef<HTMLPreElement>(null)
 
   useEffect(() => {
-    window.requestAnimationFrame(renderLoop)
+    requestAnimationFrame(renderLoop)
   }, [])
 
   const renderLoop = () => {
     preRef.current!.textContent = universe.render()
 
-    universe.tick()
-
-    requestAnimationFrame(renderLoop)
+    setTimeout(() => {
+      universe.tick()
+      requestAnimationFrame(renderLoop)
+    }, 1000 / fps)
   }
 
   return <pre ref={preRef}></pre>
